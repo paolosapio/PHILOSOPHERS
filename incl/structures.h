8380176a8d5c9a961ec430cd203949139b81e154
Paolo Sapio <psapio@c2r19s5.42madrid.com>
2025-03-28 20:12:05 +0100

    info raul

--- incl/structures.h
+++ incl/structures.h
@@ -9,23 +9,28 @@ typedef struct	s_philo		t_philo;
 // lista de filosofos
 typedef struct s_philo
 {
-	int				id_philo; //! NUMERO IIDENTIFICACION DE FILOSOFOS
-	long			last_meal_start;//comienzo de la ultima comida del filosofo o empiezo del programa
-	pthread_t		id_thread;//se inicializa con la funcion pthread_create
-	t_data_pack		*data;
+	int				id_philo; 		 //! NUMERO IIDENTIFICACION DE FILOSOFOS
+	long			last_meal_start; //comienzo de la ultima comida del filosofo o empiezo del programa
+	pthread_t		id_thread;		 //se inicializa con la funcion pthread_create
+	t_config		*config;
 	pthread_mutex_t	*fork_left;
 	pthread_mutex_t	*fork_right;
 }					t_philo;
+
+typedef struct s_config
+{
+	long			time_to_die;	//! TIEMPO DE VIDA
+	long			time_to_eat;	//! TIEMPO PARA COMER
+	long			time_to_sleep;	//! TIEMPO PARA DORMIR
+	long			n_must_eat;		//! (OPCIONAL) NUMERO DE PLATOS MINIMO QUE TIENE QUE COMER CADA FILOSOFO
+}				t_config;
 
 // structura con todos los datos
 typedef struct s_data_pack
 {
 	long			timestamp_in_ms;//! TIEMPO DE EMPIEZO DEL PROGRAMA
 	long			n_philos;		//! NUMERO TOTAL DE FILOSOFOS
-	long			time_to_die;	//! TIEMPO DE VIDA
-	long			time_to_eat;	//! TIEMPO PARA COMER
-	long			time_to_sleep;	//! TIEMPO PARA DORMIR
-	long			n_must_eat;		//! (OPCIONAL) NUMERO DE PLATOS MINIMO QUE TIENE QUE COMER CADA FILOSOFO
+	t_config		*config;
 	pthread_mutex_t	*forks;
 	t_philo 		*array_of_philosophers;
 }				t_data_pack;
@@ -34,3 +39,23 @@ typedef struct s_data_pack
 long    time_ms(void);
 void    wait_ms(long time_wait);
 #endif
+
+
+/* 
+[porque es el philo 0]
+su estructura será inicializada correctamente, se inicializa un puntero al tenedor
+izquierdo que en el array de forks será el 0
+
+[porque ya no es el 0]
+iniciará su estructura y el tenedor izquierdo, además, copiará ese puntero al philo_actual - 1, que el derecho es ese mismo. 
+En el array de forks sera el id de philo.
+
+[porque es el último]
+iniciará su estructura y el tenedor izquierdo, además, copiará ese puntero al philo_actual - 1, que el derecho es ese mismo.
+y mi derecho es el izquierdo del 0
+En el array de forks sera el id de philo.
+
+
+comienza la función que creará cada hilo y le dará como parámetro la estructura philo.
+
+fin */
